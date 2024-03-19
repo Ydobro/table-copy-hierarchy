8e16d2132a3b95f560a9efe4de6b143be46da987
Yury Dobrovolsky <ydobrovolskii@mail.ru>
2024-03-19 09:26:02 +0200

    Add base path

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,12 @@ import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(() => ({
+  base: '/table-copy-hierarchy/',
   plugins: [vue(), VueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+}))
